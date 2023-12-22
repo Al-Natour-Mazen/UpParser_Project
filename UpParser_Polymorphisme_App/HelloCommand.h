45497c13d61f05d292551dea04633703e9064153
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2023-12-22 23:31:54 +0100

    fix helloCommand include

--- UpParser_Polymorphisme_App/HelloCommand.h
+++ UpParser_Polymorphisme_App/HelloCommand.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#include "Command.h"
+#include "../UpParser_Polymorphisme_Lib/Command.h"
 
 class HelloCommand : public Command {
 private:
     std::string nameToHello;
 public:
     HelloCommand();
-    void processArgs(const std::vector<std::string>& args) override;
-    void execute() override;
+    void processArgs(const std::vector<std::string>& args);
+    void execute();
 };
 
 
