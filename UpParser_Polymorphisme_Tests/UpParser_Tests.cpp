cd1efc7a8ec802606c73e6135a8c709672e5281d
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2023-12-22 15:30:51 +0100

    add some more tests and fix it

--- UpParser_Polymorphisme_Tests/UpParser_Tests.cpp
+++ UpParser_Polymorphisme_Tests/UpParser_Tests.cpp
@@ -2,29 +2,74 @@
 #include "gtest/gtest.h"
 #include "../UpParser_Polymorphisme_Lib/HelpCommand.h"
 
-class Toto : public Command {
-public:
-    Toto() : Command("TOTO", { "-toto" }, 0, "Print 'Toto Tata Titi !'", false, true) {}
-    void processArgs(const std::vector<std::string>& args)  {}
-    void execute()  {
-        std::cout << "Toto Tata Titi !" << std::endl;
-    }
-};
 
-class HelloCommand : public Command {
-private:
-    std::string nameToHello;
-public:
-    HelloCommand() : Command("hello", { "-hello" }, 1, "Print 'Hello, [Arg] !'", true, false) {}
+namespace COO_Parser_Classes_Tests {
+    class Toto : public Command {
+    public:
+        Toto() : Command("TOTO", { "-toto" }, 0, "Print 'Toto Tata Titi !'", false, true) {}
+        void processArgs(const std::vector<std::string>& args) {}
+        void execute() {
+            std::cout << "Toto Tata Titi !" << std::endl;
+        }
+    };
 
-    void processArgs(const std::vector<std::string>& args) {
-        nameToHello = args[0];
-    }
+    class HelloCommand : public Command {
+    private:
+        std::string nameToHello;
+    public:
+        HelloCommand() : Command("hello", { "-hello" }, 1, "Print 'Hello, [Arg] !'", true, false) {}
 
-    void execute() {
-        std::cout << "Hello, " << nameToHello << "!" << std::endl;
-    }
-};
+        void processArgs(const std::vector<std::string>& args) {
+            nameToHello = args[0];
+        }
+
+        void execute() {
+            std::cout << "Hello, " << nameToHello << "!" << std::endl;
+        }
+    };
+
+    // Global variable to record the order of command execution.
+    std::vector<std::string> executionOrder;
+
+    class TestCommand : public Command {
+    private:
+        std::string commandName;
+    public:
+        TestCommand(const std::string& name) : Command(name, { "-" + name }, 1, name + " [args]", false, false), commandName(name) {}
+
+        void processArgs(const std::vector<std::string>& args) {}
+
+        void execute() {
+            // Save the command name in the execution order.
+            executionOrder.push_back(commandName);
+        }
+    };
+
+    class AddCommand : public Command {
+    private:
+        int number1;
+        int number2;
+    public:
+        AddCommand() : Command("Add", { "-add" }, 2, "add [args]", false, false), number1(0), number2(0) {}
+
+        void processArgs(const std::vector<std::string>& args) {
+            try {
+                number1 = std::stoi(args[0]);
+                number2 = std::stoi(args[1]);
+            }
+            catch (std::invalid_argument& e) {
+                throw std::runtime_error("Les arguments doivent être des nombres valides.");
+            }
+        }
+
+        void execute() {
+            int sum = number1 + number2;
+            std::cout << "La somme est " << sum << std::endl;
+        }
+    };
+}
+
+using namespace COO_Parser_Classes_Tests;
 
 namespace COO_Parser_Tests {
 
@@ -111,16 +156,14 @@ namespace COO_Parser_Tests {
         char* argv[] = { "executableExecuteCommande", "-hello", "mazen", "file1"};
         int argc = sizeof(argv) / sizeof(argv[0]);
 
-        // Call parseCommandLine to simulate the execution of the command.
-        parsing.parseCommandLine(argc, argv);
 
         // Capture the output of the execute function.
         std::ostringstream outputCapture;
         std::streambuf* coutBuffer = std::cout.rdbuf();
         std::cout.rdbuf(outputCapture.rdbuf());
-
-        // Call the execute function.
-        hello.execute();
+       
+        // Call parseCommandLine to simulate the execution of the command.
+        parsing.parseCommandLine(argc, argv);
 
         // Restore the standard output buffer.
         std::cout.rdbuf(coutBuffer);
@@ -128,4 +171,48 @@ namespace COO_Parser_Tests {
         // Check if the output of the execute function is equal to the expected value.
         ASSERT_EQ(outputCapture.str(), "Hello, mazen!\n");
     }
+
+    // Test for executing a command and checking for an error (negative case).
+    TEST(ParsingTest, ExecuteAddCommandError) {
+        Parsing parsing(true);
+
+        AddCommand add;
+        parsing.addCommand(&add);
+
+        char* argv[] = { "executableExecuteCommande", "-add", "3", "not_a_number" };
+        int argc = sizeof(argv) / sizeof(argv[0]);
+
+        // Call parseCommandLine to simulate the execution of the command.
+        try {
+            parsing.parseCommandLine(argc, argv);
+           FAIL() << "Expected std::runtime_error";
+        }
+        catch (std::runtime_error const& err) {
+            EXPECT_EQ(err.what(), std::string("Les arguments doivent être des nombres valides."));
+        }
+        catch (...) {
+            FAIL() << "Expected std::runtime_error";
+        }
+    }
+
+    TEST(ParsingTest, CommandExecutionOrder) {
+        Parsing parsing;
+
+        TestCommand command1("command1");
+        TestCommand command2("command2");
+        parsing.addCommand(&command1);
+        parsing.addCommand(&command2);
+
+        char* argv[] = { "executable", "-command1", "arg1", "-command2", "arg2", "file"};
+        int argc = sizeof(argv) / sizeof(argv[0]);
+
+        // Call parseCommandLine to simulate the execution of the commands.
+        parsing.parseCommandLine(argc, argv);
+
+        // Check if the commands were executed in the correct order.
+        ASSERT_EQ(executionOrder.size(), 2);
+        ASSERT_EQ(executionOrder[0], "command1");
+        ASSERT_EQ(executionOrder[1], "command2");
+    }
+
 }
